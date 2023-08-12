205ec93450d564e7a5c17118b5c6800a58aec554
Aakibmatin <124870240+Aakibmatin@users.noreply.github.com>
2023-08-12 17:22:01 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Addition of 3 and 4 is:");
+		int num1=3;
+		int num2=4;
+		int sum=num1+num2;
+		System.out.println(sum);
+        
+	}
+
+}
